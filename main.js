ffa81ebd3cc3ff4919889a6a7e74098930a793a0
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2020-06-15 10:42:31 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+
+
+const dicaBt = document.querySelector('#dica');
+const ajudaBt = document.querySelector('#ajuda');
+
+const dicactnr = document.querySelector('#dica_ctnr');
+const ajudactnr = document.querySelector('#ajuda_ctnr');
+
+ajudactnr.innerHTML = '<span>Lembrando que, quando conseguir decifrar este documento e identificar alguma pista ou código secreto, clique em AVANÇAR e digite o código encontrado na página seguinte, que vai pedir uma senha.</span><span>Se seu palpite estiver certo, a página vai abrir e pode nos relevar mais informações! Se não estiver certo, vai continuar pedindo uma senha.</span><span>Volte e reexamine as informações até ter certeza do código correto, lembrando que as senhas são sempre em minúsculas, sem acentos e sem espaços, tá bom?</span><span>Caso precise de alguma ajuda, clique no botão <DICA> e poderemos lhe fornecer alguma informação útil.</span><span> Se não conseguir ou preferir ir para a próxima etapa diretamente, clique no botão <RESPOSTA> e lhe daremos a senha correta da página seguinte.</span><div class="fechar"></div>'
+
+let ajudaShowing = false;
+
+ajudaBt.addEventListener('click', function() {
+    toggleAjuda()
+})
+
+let dicaShowing = false;
+
+dicaBt.addEventListener('click', function() {
+    toggleDica()
+})
+
+
+const fechar = document.querySelectorAll('.fechar');
+
+for (i = 0; i < fechar.length; ++i) {
+    fechar[i].addEventListener('click', function() {
+        ajudactnr.style.opacity = 0;
+        ajudactnr.style.visibility = 'hidden';
+        ajudactnr.style.transform = 'translateY(2rem)'
+        dicactnr.style.opacity = 0;
+        dicactnr.style.visibility = 'hidden';
+        dicactnr.style.transform = 'translateY(2rem)'
+        if(respostactnr) {
+            senhaShowing = false;
+            respostactnr.style.opacity = 0;
+            respostactnr.style.visibility = 'hidden';
+            respostactnr.style.transform = 'translateY(2rem)'
+        }
+    });
+}
+
+function toggleDica() {
+    ajudaShowing = false;
+    ajudactnr.style.opacity = 0;
+    ajudactnr.style.visibility = 'hidden';
+    ajudactnr.style.transform = 'translateY(2rem)'
+    if(dicaShowing == false) {
+        dicaShowing = true;
+        dicactnr.style.opacity = 1;
+        dicactnr.style.visibility = 'visible';
+        dicactnr.style.transform = 'translateY(0)'
+    } else {
+        dicaShowing = false;
+        dicactnr.style.opacity = 0;
+        dicactnr.style.visibility = 'hidden';
+        dicactnr.style.transform = 'translateY(2rem)'
+    }
+    if(respostactnr) {
+        senhaShowing = false;
+        respostactnr.style.opacity = 0;
+        respostactnr.style.visibility = 'hidden';
+        respostactnr.style.transform = 'translateY(2rem)'
+    }
+}
+
+function toggleAjuda() {
+    dicaShowing = false;
+    dicactnr.style.opacity = 0;
+    dicactnr.style.visibility = 'hidden';
+    dicactnr.style.transform = 'translateY(2rem)'
+    if(ajudaShowing == false) {
+        ajudaShowing = true;
+        ajudactnr.style.opacity = 1;
+        ajudactnr.style.visibility = 'visible';
+        ajudactnr.style.transform = 'translateY(0)'
+    } else {
+        ajudaShowing = false;
+        ajudactnr.style.opacity = 0;
+        ajudactnr.style.visibility = 'hidden';
+        ajudactnr.style.transform = 'translateY(2rem)'
+    }
+    if(respostactnr) {
+        senhaShowing = false;
+        respostactnr.style.opacity = 0;
+        respostactnr.style.visibility = 'hidden';
+        respostactnr.style.transform = 'translateY(2rem)'
+    }
+}
---
+++ senha.js
@@ -0,0 +1,67 @@
+const senha = document.querySelector('#senha');
+const proxPagina = document.querySelector('#prox_pagina');
+
+const inputSenha = document.querySelector('#input_senha');
+const inputAnchor = document.querySelector('#input_anchor')
+
+const proximo = document.querySelector('#proximo');
+const proximoA = document.querySelector('#proximo a');
+
+const indicadorReposta = document.querySelector('#comecar_divs > span');
+
+inputSenha.focus();
+
+inputSenha.addEventListener('input', function (evt) {
+    console.log(inputSenha.value)
+
+    if(inputSenha.value == senha.getAttribute('title')) {
+        inputAnchor.setAttribute('href', proxPagina.getAttribute('title'));
+
+        setTimeout(() => {
+            proximo.style.backgroundColor = "limegreen";
+            proximoA.style.backgroundColor = "limegreen";
+    
+            indicadorReposta.textContent = "VOCÊ ACERTOU!";
+            indicadorReposta.style.color = "limegreen";
+        }, 100);
+    } else {
+        inputAnchor.setAttribute('href', '#');
+        indicadorReposta.textContent = "DESVENDOU?";
+        indicadorReposta.style.color = "#00bab5";
+        
+        proximo.style.backgroundColor = "#00bab5";
+        proximoA.style.backgroundColor = "#00bab5";
+    }
+});
+
+inputAnchor.addEventListener('click', function() {
+
+    if(inputSenha.value == senha.getAttribute('title')) {
+        inputAnchor.setAttribute('href', proxPagina.getAttribute('title'));
+        proximo.style.backgroundColor = "limegreen"
+        proximoA.style.backgroundColor = "limegreen"
+    } else {
+        proximo.style.backgroundColor = "#E44"
+        proximoA.style.backgroundColor = "#E44"
+        inputSenha.value = ""
+    }
+})
+
+const revelar = document.querySelector('#revelar_senha div');
+const respostactnr = document.querySelector('#resposta_ctnr');
+
+let senhaShowing = false;
+
+revelar.addEventListener('click', function() {
+    if(senhaShowing == false) {
+        senhaShowing = true;
+        respostactnr.style.opacity = 1;
+        respostactnr.style.visibility = 'visible';
+        respostactnr.style.transform = 'translateY(0)'
+    } else {
+        senhaShowing = false;
+        respostactnr.style.opacity = 0;
+        respostactnr.style.visibility = 'hidden';
+        respostactnr.style.transform = 'translateY(2rem)'
+    }
+})
