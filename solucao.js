2b265cf9a1b03c950a0d04b5e63f1960535e53c4
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2020-07-01 12:49:40 -0300

    Add files via upload

---
+++ solucao.js
@@ -0,0 +1,6 @@
+
+setTimeout(() => {
+    document.querySelector('#porta_solucao').style.transform = 'rotateY(130deg)';
+
+    document.querySelector('#solution_main').style.transform = 'scale(1)';
+}, 300);
